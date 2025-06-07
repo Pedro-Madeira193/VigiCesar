a280efcb8c44dd71b6fa99e96bad0a788601cd0a
lnismo <creeperhenriquemadeira@gmail.com>
2025-06-06 22:23:53 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,52 @@ import Cesar from './components/Cesar'
 
 function App() {
 
-  function cesarCript(val){
-    return val;
+  function cesarCript(text, key){
+    let res = "";
+    text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    text = text.toLowerCase();
+    text = text.replace(/[^a-zA-Z]/g, '');
+    for(let i = 0;i < text.length;i++){
+      let temp = text.charCodeAt(i);
+      if(temp > 123 || temp < 97){
+        return "Por favor utilize letras de A-Z";
+      }
+      if(key < 0){
+        return "valor da chave deve ser igual ou maior que 0";
+      }
+      key = parseInt(key);
+      if(!Number.isInteger(key)){
+        return "chave deve ser um número inteiro";
+      }
+      temp = ((temp - 97 + key) % 26) + 97;
+      temp = String.fromCharCode(temp);
+      res = res + temp;
+    }
+    return res.replaceAll("-64"," ");
   }
 
-  function cesarDescript(val){
-    return val
+  function cesarDescript(text, key){
+    let res = "";
+    text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    text = text.toLowerCase();
+    text = text.replace(/[^a-zA-Z]/g, '');
+    for(let i = 0;i < text.length;i++){
+      let temp = text.charCodeAt(i);
+      if(temp > 123 || temp < 97){
+        return "Por favor utilize letras de A-Z";
+      }
+      if(key < 0){
+        return "valor da chave deve ser igual ou maior que 0";
+      }
+      key = parseInt(key);
+      if(!Number.isInteger(key)){
+        return "chave deve ser um número inteiro";
+      }
+      temp = ((temp - 97 - key + 26) % 26) + 97;
+      temp = String.fromCharCode(temp);
+      res = res + temp;
+    }
+    return res.replaceAll("-64"," ");
   }
 
   return (
@@ -28,8 +68,8 @@ function App() {
           <a href='#Vigenere' className='mainBtns' id='vigenereBtn'>vigenere</a>
         </div>
       </div>
-      <Cesar name="Cesar" title="Cifra de César" cript={cesarCript} descript={cesarDescript}/>
-      <Cesar name="Vigenere" title="Cifra de Vigenére" cript={cesarCript} descript={cesarDescript}/>
+      <Cesar name="Cesar" title="Cifra de César" cript={cesarCript} descript={cesarDescript} keyPlaceholder='número chave'/>
+      <Cesar name="Vigenere" title="Cifra de Vigenére" cript={cesarCript} descript={cesarDescript} keyPlaceholder='texto chave'/>
     </div>
   );
 }
--- src/components/Cesar.js
+++ src/components/Cesar.js
@@ -1,9 +1,10 @@
 import './Cesar.css'
 import { useState } from 'react';
 
-function Cesar({name, title, cript, descript}) {
+function Cesar({name, title, cript, descript, keyPlaceholder}) {
     const [textToCript, setTextToCript] = useState('');
     const [criptToText, setCriptToText] = useState('');
+    const [key, setKey] = useState(0);
 
     return(
         <div className="container" id={name}>
@@ -11,14 +12,15 @@ function Cesar({name, title, cript, descript}) {
                 <h1 className='title'>{title}</h1>
             </div>
             <div className='main'>
+                <input type='text' placeholder={keyPlaceholder} className='chave' onChange={(e) => setKey(e.target.value)}></input>
                 <div className='inputsContainer'>
                     <div className='textToCript'>
                         <textarea placeholder='texto para criptografia' style={{resize: "none"}} onChange={(e) => setTextToCript(e.target.value)} value={textToCript}></textarea>
-                        <button type='button' className='cesarBtn' onClick={() => setCriptToText(cript(textToCript))}>criptografar</button>
+                        <button type='button' className='cesarBtn' onClick={() => setCriptToText(cript(textToCript,key))}>criptografar</button>
                     </div>
                     <div className='criptToText'>
                         <textarea placeholder='criptografia para texto' style={{resize: "none"}} onChange={(e) => setCriptToText(e.target.value)} value={criptToText}></textarea>
-                        <button type='button' className='cesarBtn' onClick={() => setTextToCript(descript(criptToText))}>descriptografar</button>
+                        <button type='button' className='cesarBtn' onClick={() => setTextToCript(descript(criptToText,key))}>descriptografar</button>
                     </div>
                 </div>
             </div>
