f3299f9b622966a4a57690571ada43b54c6a44c6
lnismo <creeperhenriquemadeira@gmail.com>
2025-07-04 22:08:58 -0300

    adjusting bugs

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ function App() {
       <header className='container' id="teste">
         <h1>VigiCesar</h1>
         <span>descriptador e encriptador</span>
-        <a href='#menu' id="downPage"><i class="bi bi-chevron-double-down"></i></a>
+        <a href='#Cesar' className='downPage' id='cesarBtn'><i className='bi bi-chevron-double-down'></i></a>
       </header>
       <div id="menu" className='container'>
         <h2>escolha qual deseja decifrar/cifrar:</h2>
